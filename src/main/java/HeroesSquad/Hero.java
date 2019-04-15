4af5681d6ed922722a90ce4f8c757cf9fa6afad0
System Administrator <root@Owners-MacBook-Pro.local>
2019-04-15 11:45:10 +0300

    create tests for intstantiating getter methods for Hero class

--- src/main/java/HeroesSquad/Hero.java
+++ src/main/java/HeroesSquad/Hero.java
@@ -1,4 +1,37 @@
 package HeroesSquad;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    public String gender;
+
+    public Hero(String name, int age, String specialPower, String weakness, String gender) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
--- src/test/java/HeroesSquad/HeroTest.java
+++ src/test/java/HeroesSquad/HeroTest.java
@@ -1,5 +1,38 @@
 package HeroesSquad;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+        Hero myHero = new Hero("Superman",50, "Flight","Kryptonite", "Male");
+        assertEquals(true, myHero instanceof Hero);
+    }
+    @Test
+    public void Hero_instantiatesWithName_String() {
+        Hero myHero = new Hero("Batman", 50,"Running", "Everything", "Male");
+        assertEquals("Batman", myHero.getName());
+    }
+    @Test
+    public void Hero_instantiatesWithAge_Int() {
+        Hero myHero = new Hero("Batman", 50,"Running", "Everything", "Male");
+        assertEquals(50, myHero.getAge());
+    }
+    @Test
+    public void Hero_instantiatesWithSpecialPower_String() {
+        Hero myHero = new Hero("Batman", 50,"Running", "Everything", "Male");
+        assertEquals("Running", myHero.getSpecialPower());
+    }
+    @Test
+    public void Hero_instantiatesWithWeakness_String() {
+        Hero myHero = new Hero("Batman", 50,"Running", "Everything", "Male");
+        assertEquals("Eberything", myHero.getWeakness());
+    }
+    @Test
+    public void Hero_instantiatesWithGender_String() {
+        Hero myHero = new Hero("Batman", 50,"Running", "Everything", "Male");
+        assertEquals("Male", myHero.getGender());
+    }
+
 
 }
